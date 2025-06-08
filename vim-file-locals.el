3d5940e99f07e8cc157e6a1ed02a066dc6b67679
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-06-08 15:57:16 +0200

    refactor!: change the package name

--- vim-file-locals.el
+++ vim-file-locals.el
@@ -1,11 +1,11 @@
-;;; vim-modelines.el --- Set editor options from Vim modeline
+;;; vim-file-locals.el --- Set editor options from Vim modeline
 
 ;; Copyright (C) 2025  Abdelhak Bougouffa
 
 ;; Author: Abdelhak Bougouffa
 ;; Keywords: files, text, emulations, matching
 ;; Version: 0.2.1
-;; URL: https://github.com/abougouffa/vim-modelines
+;; URL: https://github.com/abougouffa/vim-file-locals
 ;; Package-Requires: ((emacs "30.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -31,59 +31,64 @@
 
 (require 'editorconfig)
 
-(defgroup vim-modelines nil
+(defgroup vim-file-locals nil
   "Set some Emacs' editor options form Vim's modelines."
   :group 'editing)
 
-(defcustom vim-modelines-modelines 5
+(defcustom vim-file-locals-modelines 5
   "Number of lines to consider when looking for modelines in the beginning and the end of the buffer."
   :type 'integer
-  :group 'vim-modelines)
+  :group 'vim-file-locals)
 
-(defcustom vim-modelines-verbose nil
+(defcustom vim-file-locals-prioritize-emacs-prop-line t
+  "Prioritize Emacs prop-line options over Vim's modeline ones."
+  :type 'boolean
+  :group 'vim-file-locals)
+
+(defcustom vim-file-locals-verbose nil
   "Verbose mode."
   :type 'boolean
-  :group 'vim-modelines)
+  :group 'vim-file-locals)
 
-(defcustom vim-modelines-before-apply-hook nil
-  "Runs after setting `vim-modelines-buffer-options' and before applying options."
+(defcustom vim-file-locals-before-apply-hook nil
+  "Runs after setting `vim-file-locals-buffer-options' and before applying options."
   :type '(repeat function)
-  :group 'vim-modelines)
+  :group 'vim-file-locals)
 
-(defcustom vim-modelines-after-apply-hook nil
+(defcustom vim-file-locals-after-apply-hook nil
   "A hook to run after applying options."
   :type '(repeat function)
-  :group 'vim-modelines)
+  :group 'vim-file-locals)
 
-(defcustom vim-modelines-options-alist
-  '((("filetype" "ft" "syntax" "syn") . vim-modelines-filetype)
-    (("shiftwidth" "sw") . vim-modelines-shiftwidth)
-    (("textwidth" "tw") . vim-modelines-textwidth)
-    (("tabstop" "ts" "softtabstop" "sts") . vim-modelines-tabstop)
-    (("number" "nu" "nonumber" "nonu") . vim-modelines-number)
-    (("expandtab" "et" "noexpandtab" "noet") . vim-modelines-expandtab)
-    (("readonly" "ro" "modifiable" "ma") . vim-modelines-readonly)
-    (("linebreak" "lbr" "nolinebreak" "nolbr") . vim-modelines-linebreak)
-    (("smartindent" "si" "nosmartindent" "nosi" "autoindent" "ai" "noautoindent" "noai") . vim-modelines-smartindent)
-    (("encoding" "enc") . vim-modelines-encoding)
-    (("fileencoding" "fenc" "fileformat" "ff") . vim-modelines-fileencoding-fileformat)
-    (("relativenumber" "rnu" "norelativenumber" "nornu") . vim-modelines-relativenumber))
+(defcustom vim-file-locals-options-alist
+  '((("filetype" "ft" "syntax" "syn") . vim-file-locals-filetype)
+    (("shiftwidth" "sw") . vim-file-locals-shiftwidth)
+    (("textwidth" "tw") . vim-file-locals-textwidth)
+    (("tabstop" "ts" "softtabstop" "sts") . vim-file-locals-tabstop)
+    (("number" "nu" "nonumber" "nonu") . vim-file-locals-number)
+    (("expandtab" "et" "noexpandtab" "noet") . vim-file-locals-expandtab)
+    (("readonly" "ro" "modifiable" "ma") . vim-file-locals-readonly)
+    (("linebreak" "lbr" "nolinebreak" "nolbr") . vim-file-locals-linebreak)
+    (("smartindent" "si" "nosmartindent" "nosi" "autoindent" "ai" "noautoindent" "noai") . vim-file-locals-smartindent)
+    (("encoding" "enc") . vim-file-locals-encoding)
+    (("fileencoding" "fenc" "fileformat" "ff") . vim-file-locals-fileencoding-fileformat)
+    (("relativenumber" "rnu" "norelativenumber" "nornu") . vim-file-locals-relativenumber))
   "Vim modeline options and their handler functions."
   :type '(alist :key-type ((repeat symbol) :tag "Option and aliases") :value-type (function :tag "Handler function"))
-  :group 'vim-modelines)
+  :group 'vim-file-locals)
 
-(defun vim-modelines--log (fmt &rest args)
-  (when vim-modelines-verbose
-    (apply #'message (append (list (concat "vim-modelines: " fmt)) args))))
+(defun vim-file-locals--log (fmt &rest args)
+  (when vim-file-locals-verbose
+    (apply #'message (append (list (concat "vim-file-locals: " fmt)) args))))
 
-(defun vim-modelines-no-p (name)
+(defun vim-file-locals-no-p (name)
   (and (string-prefix-p "no" name) t))
 
-(defvar-local vim-modelines-buffer-options nil)
-(defun vim-modelines-buffer-option (&rest names)
-  (seq-some (lambda (name) (assoc name vim-modelines-buffer-options)) names))
+(defvar-local vim-file-locals-buffer-options nil)
+(defun vim-file-locals-buffer-option (&rest names)
+  (seq-some (lambda (name) (assoc name vim-file-locals-buffer-options)) names))
 
-(defun vim-modelines-extract-region (beg end)
+(defun vim-file-locals-extract-region (beg end)
   "Extract the options form region between BEG and END."
   (save-excursion
     (save-restriction
@@ -96,100 +101,100 @@
              ((re-search-forward "[[:space:]]+\\(?:vi:\\|vim:\\|ex:\\)[[:space:]]?\\(.*\\)$" nil t)
               (string-split (match-string-no-properties 1) "[[:space:]:]" t)))))))
 
-(defun vim-modelines-extract ()
+(defun vim-file-locals-extract ()
   "Extract the options from the current buffer."
   (save-excursion
     (append
-     (vim-modelines-extract-region (goto-char (point-min)) (line-end-position (1+ vim-modelines-modelines)))
+     (vim-file-locals-extract-region (goto-char (point-min)) (line-end-position (1+ vim-file-locals-modelines)))
      (let ((pos (goto-char (point-max))))
-       (vim-modelines-extract-region (line-beginning-position (- (1- vim-modelines-modelines))) pos)))))
+       (vim-file-locals-extract-region (line-beginning-position (- (1- vim-file-locals-modelines))) pos)))))
 
 ;;;###autoload
-(defun vim-modelines-apply ()
+(defun vim-file-locals-apply ()
   "Apply the options in the current buffer."
-  (when-let* ((options (vim-modelines-extract)))
+  (when-let* ((options (vim-file-locals-extract)))
     ;; Move "filetype" to the beginning so the mode is applied before other options
     (when-let* ((ft (assoc nil options (lambda (key _k) (member key '("filetype" "ft" "syntax" "syn"))))))
       (setq options (cons ft (assoc-delete-all (car ft) options))))
-    (setq vim-modelines-buffer-options options)
-    (run-hooks 'vim-modelines-before-apply-hook)
-    (dolist (opt vim-modelines-buffer-options)
+    (setq vim-file-locals-buffer-options options)
+    (run-hooks 'vim-file-locals-before-apply-hook)
+    (dolist (opt vim-file-locals-buffer-options)
       (when-let* ((name (car opt))
                   (name (string-trim name))
-                  (handler (cdr (assoc name vim-modelines-options-alist (lambda (keys key) (member key keys))))))
+                  (handler (cdr (assoc name vim-file-locals-options-alist (lambda (keys key) (member key keys))))))
         (let* ((value (cdr opt))
                (value (and value (string-trim value))))
-          (vim-modelines--log "setting %s%s" name (if value (format " to %s" value) ""))
+          (vim-file-locals--log "setting %s%s" name (if value (format " to %s" value) ""))
           (funcall handler name value))))
-    (run-hooks 'vim-modelines-after-apply-hook)))
+    (run-hooks 'vim-file-locals-after-apply-hook)))
 
-(defun vim-modelines-tabstop (name &optional value)
+(defun vim-file-locals-tabstop (name &optional value)
   (when-let* ((offset (ignore-errors (string-to-number value))))
     (editorconfig-set-indentation nil value)))
 
-(defun vim-modelines-shiftwidth (name &optional value)
+(defun vim-file-locals-shiftwidth (name &optional value)
   (when-let* ((offset (ignore-errors (string-to-number value))))
     (editorconfig-set-indentation nil nil value)))
 
-(defun vim-modelines-textwidth (name &optional value)
+(defun vim-file-locals-textwidth (name &optional value)
   (when-let* ((width (ignore-errors (string-to-number value))))
     (setq fill-column width)))
 
-(defun vim-modelines-number (name &optional _value)
-  (display-line-numbers-mode (if (vim-modelines-no-p name) -1 1)))
+(defun vim-file-locals-number (name &optional _value)
+  (display-line-numbers-mode (if (vim-file-locals-no-p name) -1 1)))
 
-(defun vim-modelines-relativenumber (name &optional _value)
-  (setq display-line-numbers (if (vim-modelines-no-p name) 'relative t))
-  (unless (vim-modelines-buffer-option "nonumber" "nonu")
+(defun vim-file-locals-relativenumber (name &optional _value)
+  (setq display-line-numbers (if (vim-file-locals-no-p name) 'relative t))
+  (unless (vim-file-locals-buffer-option "nonumber" "nonu")
     (display-line-numbers-mode 1)))
 
-(defun vim-modelines-expandtab (name &optional _value)
-  (setq indent-tabs-mode (vim-modelines-no-p name)))
+(defun vim-file-locals-expandtab (name &optional _value)
+  (setq indent-tabs-mode (vim-file-locals-no-p name)))
 
-(defun vim-modelines-filetype (name &optional value)
+(defun vim-file-locals-filetype (name &optional value)
   (when-let* ((value (car (string-split value "\\."))) ; In values like ":set ft=c.doxygen", ignore the second type
               (mode (alist-get (file-name-with-extension "dummy" value) auto-mode-alist nil nil #'string-match-p)))
-    (vim-modelines--log "inferred major mode for %s=%s: %S" name value mode)
+    (vim-file-locals--log "inferred major mode for %s=%s: %S" name value mode)
     ;; Don't lose the saved options after applying the mode
-    (let ((state (buffer-local-set-state vim-modelines-buffer-options vim-modelines-buffer-options)))
+    (let ((state (buffer-local-set-state vim-file-locals-buffer-options vim-file-locals-buffer-options)))
       (funcall mode)
       (buffer-local-restore-state state))))
 
-(defun vim-modelines-readonly (name &optional value)
+(defun vim-file-locals-readonly (name &optional value)
   (when (or (and (member name '("readonly" "ro")) (equal value "on"))
             (and (member name '("modifiable" "ma")) (equal value "off")))
     (read-only-mode 1)))
 
-(defun vim-modelines-linebreak (name &optional _value)
+(defun vim-file-locals-linebreak (name &optional _value)
   (cond ((member name '("linebreak" "lbr"))
          (visual-line-mode 1)
          (when (fboundp 'visual-wrap-prefix-mode) ; Emacs 30
-           (cond ((vim-modelines-buffer-option "breakindent" "bri")
-                  (vim-modelines--log "setting breakindent")
+           (cond ((vim-file-locals-buffer-option "breakindent" "bri")
+                  (vim-file-locals--log "setting breakindent")
                   (visual-wrap-prefix-mode 1))
-                 ((vim-modelines-buffer-option "nobreakindent" "nobri")
-                  (vim-modelines--log "setting nobreakindent")
+                 ((vim-file-locals-buffer-option "nobreakindent" "nobri")
+                  (vim-file-locals--log "setting nobreakindent")
                   (visual-wrap-prefix-mode -1)))))
         ((member name '("nolinebreak" "nolbr"))
          (visual-line-mode -1))))
 
-(defun vim-modelines-smartindent (name &optional _value)
-  (electric-indent-mode (if (vim-modelines-no-p name) -1 1)))
+(defun vim-file-locals-smartindent (name &optional _value)
+  (electric-indent-mode (if (vim-file-locals-no-p name) -1 1)))
 
-(defun vim-modelines-encoding (name &optional value)
+(defun vim-file-locals-encoding (name &optional value)
   (when-let* ((enc (unless (string-empty-p value) (intern value)))
               ((memq enc (coding-system-list 'base-only))))
     (setq buffer-file-coding-system enc)))
 
-(defun vim-modelines-fileencoding-fileformat (_name &optional _value)
+(defun vim-file-locals-fileencoding-fileformat (_name &optional _value)
   (let ((coding
-         (when-let* ((fenc (vim-modelines-buffer-option "fileencoding" "fenc"))
+         (when-let* ((fenc (vim-file-locals-buffer-option "fileencoding" "fenc"))
                      (val (unless (string-empty-p (cdr fenc)) (intern (downcase (cdr fenc)))))
                      (val (ignore-errors (coding-system-aliases val)))
                      (val (car (seq-intersection (coding-system-list 'base-only) val))))
            val))
         (eol
-         (when-let* ((ff (vim-modelines-buffer-option "fileformat" "ff")))
+         (when-let* ((ff (vim-file-locals-buffer-option "fileformat" "ff")))
            (pcase (cdr ff)
              ("unix" 'undecided-unix)
              ("mac" 'undecided-mac)
@@ -202,18 +207,18 @@
       ;; We lure the `editorconfig-merge-coding-systems' function to return our
       ;; encoding, then, we call `editorconfig-set-coding-system-revert' with
       ;; garbage arguments to apply that encoding to the buffer
-      (vim-modelines--log "applying encoding %S" encoding)
+      (vim-file-locals--log "applying encoding %S" encoding)
       (cl-letf (((symbol-function 'editorconfig-merge-coding-systems) (lambda (&rest _) encoding)))
         (editorconfig-set-coding-system-revert nil nil)))))
 
 ;;;###autoload
-(define-minor-mode vim-modelines-mode
+(define-minor-mode vim-file-locals-mode
   "Enable support for Vim's modeline options."
   :global t
-  (if vim-modelines-mode
-      (add-hook 'find-file-hook #'vim-modelines-apply)
-    (remove-hook 'find-file-hook #'vim-modelines-apply)))
+  (if vim-file-locals-mode
+      (add-hook 'find-file-hook #'vim-file-locals-apply)
+    (remove-hook 'find-file-hook #'vim-file-locals-apply)))
 
 
-(provide 'vim-modelines)
-;;; vim-modelines.el ends here
+(provide 'vim-file-locals)
+;;; vim-file-locals.el ends here
